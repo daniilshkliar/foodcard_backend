7f20e76372ace0f977441296cada597d6777353a
daniilshkliar <daniilshkliar@yandex.ru>
2020-11-05 10:22:15 +0300

    initial commit

---
+++ backend/utils.py
@@ -0,0 +1,8 @@
+# from core.serializers import CurrentUser
+
+
+# def my_jwt_response_handler(token, user=None, request=None):
+#     return {
+#         'token': token,
+#         'user': CurrentUser(user, context={'request': request}).data
+#     }
